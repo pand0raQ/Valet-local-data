bf7f35d28a55ab7d65df04707573d6433a40309b
pand0raQ <pand0ra_Q@hotmail.com>
2023-12-21 22:19:07 +0400

    after 1 session . before adding enum

---
+++ Valet local data/localDataStorage.swift
@@ -0,0 +1,8 @@
+//
+//  localDataStorage.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 21.12.2023.
+//
+
+import Foundation
---
+++ Valet local data/storedDataView.swift
@@ -0,0 +1,8 @@
+//
+//  storedDataView.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 21.12.2023.
+//
+
+import Foundation
