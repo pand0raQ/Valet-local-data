f085fae1ea54f82c44a40162f6241cf7e92c7a67
pand0raQ <pand0ra_Q@hotmail.com>
2023-12-21 21:09:11 +0400

    Initial Commit

---
+++ Valet local data/Valet_local_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valet_local_dataApp.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Valet_local_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
