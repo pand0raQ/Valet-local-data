f792a6ce2278767b427a3d886ccd4c0e3283c5e7
pand0raQ <pand0ra_Q@hotmail.com>
2024-01-04 02:07:56 +0400

    added appetite

---
+++ Valet local data/Views/AppetiteCardView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetiteCardView.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 03.01.2024.
+//
+
+import SwiftUI
+
+struct AppetiteCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppetiteCardView()
+}
