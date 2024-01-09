5b36090d629995f513a3a35cc5568837e27b023b
pand0raQ <pand0ra_Q@hotmail.com>
2024-01-09 20:20:52 +0400

    added medication with complete working notification management

---
+++ Valet local data/DataModels/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 05.01.2024.
+//
+
+import Foundation
---
+++ Valet local data/Views/ScheduledNotificationsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduledNotificationsView.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 04.01.2024.
+//
+
+import SwiftUI
+
+struct ScheduledNotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduledNotificationsView()
+}
