14756d6bac76747071185de2060341e02e4c2bd5
pand0raQ <pand0ra_Q@hotmail.com>
2024-01-11 15:56:59 +0400

    medication card view, checkmarks, everything works

---
+++ Valet local data/Views/ScheduledMedicationsTimelineView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduledMedicationsTimelineView.swift
+//  Valet local data
+//
+//  Created by Анастасия Степаносова on 11.01.2024.
+//
+
+import SwiftUI
+
+struct ScheduledMedicationsTimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduledMedicationsTimelineView()
+}
